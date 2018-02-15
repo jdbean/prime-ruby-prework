264facf688820291295d8082894ca7f571f38d4d
J.D. Bean <jonathandbean@gmail.com>
2018-02-15 20:31:43 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -4,15 +4,13 @@ def prime?(int)
   if int > 0
     int -= 1
     range = (2..int).to_a
-    binding.pry
-
   elsif int < 0
     int += 1
     range = (int..-2).to_a
   else
     return false
   end
-
+  binding.pry
   range.each do |i|
     if int % i == 0
       return false
