37e797579572b6d3e61edf94a848f369520542ef
J.D. Bean <jonathandbean@gmail.com>
2018-02-15 20:35:47 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,13 +1,10 @@
 # require 'pry'
 
 def prime?(int)
-  if int > 0
+  if int > 1
     num = int - 1
     range = (2..num).to_a
-  elsif int < 0
-    num = int + 1
-    range = (num..-2).to_a
-  else
+  else 
     return false
   end
   range.each do |i|
